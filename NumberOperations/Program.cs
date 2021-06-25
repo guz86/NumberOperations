5a709c5cf2d8876c20be1265e524624d412c8d8a
guz86 <guzmo6@rambler.ru>
2021-06-25 19:10:44 +0300

    test

--- NumberOperations/Program.cs
+++ NumberOperations/Program.cs
@@ -7,6 +7,9 @@ namespace NumberOperations
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("test");
+            Console.WriteLine("test2");
         }
     }
 }
