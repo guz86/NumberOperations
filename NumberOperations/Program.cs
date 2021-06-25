f85331481d788b92ae71e07b8a81b02b9ab25720
guz86 <guzmo6@rambler.ru>
2021-06-25 18:49:28 +0300

    Добавьте файлы проекта.

---
+++ NumberOperations/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NumberOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
