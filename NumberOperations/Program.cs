1eb27db4e1fd9b860146e27481f8d651eb0e2133
guz86 <guzmo6@rambler.ru>
2021-06-25 23:47:52 +0300

    trycatch

--- NumberOperations/Program.cs
+++ NumberOperations/Program.cs
@@ -12,7 +12,7 @@ namespace NumberOperations
 
             // string str = "5.5";
             // double a = double.Parse(str);
-
+            /*
             string str = "5.5";
 
             NumberFormatInfo numberFormatInfo = new NumberFormatInfo()
@@ -21,9 +21,21 @@ namespace NumberOperations
             };
 
             double a = double.Parse(str, numberFormatInfo);
+            */
+
+            string str = "5,4asd";
+
             
+            try
+            {
+                double a = double.Parse(str);
+                Console.WriteLine("Успешная конвертация.");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Ошибка конвертации.");
 
-
+            }
 
         }
     }
